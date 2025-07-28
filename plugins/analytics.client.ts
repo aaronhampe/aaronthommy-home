1547149b6be0b0dac998db0abe625a272e96368a
aaronthom <hampeaaron@gmail.com>
2025-07-28 22:47:06 +0200

    final change

--- plugins/analytics.client.ts
+++
@@ -1,6 +0,0 @@
-export default defineNuxtPlugin(() => {
-  const { cookiesEnabledIds } = useCookieControl()
-  if (cookiesEnabledIds.value.includes('analytics')) {
-    import('plausible-tracker').then(({ create }) => create('aaronthommy.com'))
-  }
-})
